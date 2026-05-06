2905a643ca564f9c0fa00627064db5824af53f1f
Dachez <sidneydachez@gmail.com>
2026-05-06 15:33:48 +0200

    create branch website

---
+++ frontend/voice-recognition/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { useState } from 'react'
+import './App.css'
+import Import from './Import.jsx'
+
+
+function App() {
+
+  return(
+    <>
+      <Import/>
+    </>
+  )
+}
+
+export default App
