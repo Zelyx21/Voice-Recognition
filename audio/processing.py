e078f9f85053e10f9b6b44addb0bda3131a9e68c
Zelyx21 <beaulieu.clem.30260@gmail.com>
2026-05-06 14:58:06 +0200

    added folders to git

--- audio/processing.py
+++ audio/processing.py
@@ -1,1 +1,18 @@
+"""
+Python file that take a wav file and :
+    - resample it
+    - denoise it
+    - cut the gaps
+"""
 
+# ------------------------Fetch the file------------------------------------------------
+# Will modify for fastAPI
+name_file = "20260506_135639.wav"
+input_path = "audio\\audio_output_conversion\\" + str(name_file)
+output_path = "audio\\audio_output_processing\\" + str(name_file)
+
+# ------------------------Resample the file---------------------------------------------
+
+# ------------------------Denoise the file----------------------------------------------
+
+# ------------------------Cut the gaps------------------------------------------------
