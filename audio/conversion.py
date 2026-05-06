22d77224c3e2326ea7a7c3964a945c9b7841e593
Zelyx21 <beaulieu.clem.30260@gmail.com>
2026-05-06 16:45:22 +0200

    processing.py works

--- audio/conversion.py
+++ audio/conversion.py
@@ -4,10 +4,10 @@ Python file that converts an audio file to a .wav file
 
 import subprocess
 
-#Will modify later so it works with FastAPI
-#catch "name_file" 
-name_file = "20260506_135639.m4a"
-input_path = "audio\\audio_input\\"+str(name_file)
-output_path = "audio\\audio_output_conversion\\"+name_file.rsplit(".",1)[0] +".wav"
+# Will modify later so it works with FastAPI
+# catch "name_file"
+name_file = "test.m4a"
+input_path = "audio\\audio_input\\" + str(name_file)
+output_path = "audio\\audio_output_conversion\\" + name_file.rsplit(".", 1)[0] + ".wav"
 
-subprocess.run(["ffmpeg","-i",input_path,output_path])
+subprocess.run(["ffmpeg", "-i", input_path, output_path])
--- audio/processing.py
+++ audio/processing.py
@@ -5,14 +5,50 @@ Python file that take a wav file and :
     - cut the gaps
 """
 
+import librosa
+import soundfile as sf
+import noisereduce as nr
+from silero_vad import load_silero_vad, read_audio, get_speech_timestamps
+import numpy as np
+import torch
+
 # ------------------------Fetch the file------------------------------------------------
 # Will modify for fastAPI
-name_file = "20260506_135639.wav"
+name_file = "test.wav"
 input_path = "audio\\audio_output_conversion\\" + str(name_file)
 output_path = "audio\\audio_output_processing\\" + str(name_file)
 
 # ------------------------Resample the file---------------------------------------------
 
+audio, sr = librosa.load(input_path, sr=16000)
+
 # ------------------------Denoise the file----------------------------------------------
 
+audio_denoised = nr.reduce_noise(y=audio, sr=sr, stationary=True,prop_decrease=0.8)
+audio_denoised = nr.reduce_noise(y=audio, sr=sr, stationary=False,prop_decrease=0.8)
+
 # ------------------------Cut the gaps------------------------------------------------
+
+model_vad = load_silero_vad()
+
+wav = torch.FloatTensor(  # converts the audio to a tensor PyTorch needed for get_speech_timestamps
+    audio_denoised
+)
+# get the timestamps
+timestamps = get_speech_timestamps(wav, model_vad, return_seconds=True)
+
+# cut silences and concatenate
+clean_audio = []
+for segment in timestamps:
+    start = int(
+        segment["start"] * sr
+    )  # we multiply by sr to get the index (timestamps are in seconds and sr is the number of audio sample per sec)
+    end = int(segment["end"] * sr)
+    clean_audio.append(wav[start:end].numpy())
+
+final_audio = np.concatenate(clean_audio)
+
+# ------------------------Export the file (modify with fastAPI later)-------------------
+
+sf.write(output_path, final_audio, 16000)
+print("Finished !")
