3374b15df88331d36dbcf0cd5b083b52a2ee77f3
Zelyx21 <beaulieu.clem.30260@gmail.com>
2026-05-11 15:22:54 +0200

    api.py

--- api/api.py
+++ api/api.py
@@ -1,1 +1,27 @@
+"""
+A python file in which is built our API fastAPI
+"""
 
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from api.actions.voice_similarity import voice_similarity
+
+#Command to run univcorn
+# uvicorn api.api:app --reload
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"status":"running"}
+
+@app.post("/identify")
+async def identify(file: UploadFile = File(...)):
+    audio_bytes = await file.read()
+    return voice_similarity(audio_bytes)
