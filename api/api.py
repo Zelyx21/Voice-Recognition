4cc794bac4f338e7bd14b10f40ee6003d41f28c7
Zelyx21 <beaulieu.clem.30260@gmail.com>
2026-05-11 12:07:25 +0200

    idk

--- api/api.py
+++ api/api.py
@@ -2,3 +2,16 @@
 A python file in which is built our API fastAPI
 """
 
+from fastapi import FastAPI, UploadFile
+from api.actions.voice_similarity import voice_similarity
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status":"running"}
+
+@app.post("/identify")
+async def identify(file: UploadFile):
+    audio_bytes = await file.read()
+    return voice_similarity(audio_bytes)
