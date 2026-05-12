381f966a20bc78895fb839d65004d5a23cdd6878
Dachez <sidneydachez@gmail.com>
2026-05-12 13:07:47 +0200

    correction

--- audio/processing.py
+++ audio/processing.py
@@ -87,6 +87,12 @@ def vad(audio_denoised:np.ndarray, sr=16000):
     # get the timestamps
     timestamps = get_speech_timestamps(wav, model_vad, return_seconds=True)
 
+    print(f"timestamps : {timestamps}")
+    issue = [False, ""]
+    if len(timestamps) == 0: # if there is no voice detected, we raise an issue
+        issue = [True, "No voice detected in the audio file. Please provide a clearer audio file."]
+        return audio_denoised, issue
+    
     # cut silences and concatenate
     clean_audio = []
     for segment in timestamps:
@@ -96,4 +102,4 @@ def vad(audio_denoised:np.ndarray, sr=16000):
         end = int(segment["end"] * sr)
         clean_audio.append(wav[start:end].numpy())
 
-    return np.concatenate(clean_audio)
+    return np.concatenate(clean_audio), issue
