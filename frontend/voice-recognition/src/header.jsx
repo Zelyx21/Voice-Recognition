9008c3cf25ac35b39a8d2dacb0e47acde4ff4630
Dachez <sidneydachez@gmail.com>
2026-05-13 10:17:05 +0200

    style

--- frontend/voice-recognition/src/header.jsx
+++ frontend/voice-recognition/src/header.jsx
@@ -1,10 +1,12 @@
 
 import { useState } from 'react'
 import './App.css'
+import dellLogo from './dell_logo.png'
 
 function Header() {
   return (
     <main id="center">
+      <img src={dellLogo} alt="Logo DELL" className="logo" />
       <h1>Voice Recognition</h1>
     </main>
   )
