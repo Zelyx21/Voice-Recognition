2bb890b091fb628e0053f95c7f01fc168c654fc9
Dachez <sidneydachez@gmail.com>
2026-05-13 10:39:09 +0200

    merge conflit style && main

--- audio/conversion.py
+++ audio/conversion.py
@@ -8,11 +8,20 @@ def conversion(audio:bytes):
     """
     Takes raw audio bytes and returns wav bytes
     """
+    if is_wav(audio):
+        return audio
+    
     commande = ["ffmpeg","-i","pipe:0","-f","wav","pipe:1"]
-
     result = subprocess.run(commande, input=audio, capture_output=True)
 
     if result.returncode != 0:
         raise ValueError(f"ffmpeg error : {result.stderr.decode()}")
     
     return result.stdout
+
+def is_wav(audio_bytes: bytes):
+    return (
+        len(audio_bytes) > 12 and
+        audio_bytes[:4] == b'RIFF' and
+        audio_bytes[8:12] == b'WAVE'
+    )
