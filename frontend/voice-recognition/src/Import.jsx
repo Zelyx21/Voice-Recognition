b9cfc3aa4b252e48cb7cb64f2f955855c2f05e2c
Dachez <sidneydachez@gmail.com>
2026-05-13 10:17:05 +0200

    style

--- frontend/voice-recognition/src/Import.jsx
+++ frontend/voice-recognition/src/Import.jsx
@@ -39,7 +39,7 @@ function Import() {
         //If audioFile is empty
         <> 
           <label htmlFor="audio-upload" className="button-import">
-            Importer un fichier audio
+            Import an audio file
           </label>
           <input
             id="audio-upload"
@@ -50,23 +50,31 @@ function Import() {
         </>
       ) : (
         <div className="file-info">
-          <p>Fichier sélectionné : <strong>{audioFile.name}</strong></p>
+          <p>File selected : <strong>{audioFile.name}</strong></p>
           <button 
             className="remove-import" 
             onClick={() => setAudioFile(null)}
           >
-            Changer de fichier
+            Change file
           </button>
           <button className="send-import" onClick={sendRecording} disabled={loading}>
-            {loading ? "Analyse en cours...":"Envoyer l'enregistrement"}
+            {loading ? "Analysis in progress..." : "Voice recognition"}
+          </button>
+          <button className="Recording_data" onClick={sendRecording}>
+            Recording
           </button>
 
-          {result && (
-            <div className = "result">
-              <p>Locuteur : {result.name}</p>
+          {result && !result.issue && (
+            <div className="result">
+              <p>Speaker : {result.name}</p>
               <p>Score : {result.score}</p>
             </div>
-          )}
+            )}
+          {result && result.issue &&  (
+            <div className="issue">
+              <p>Issues : {result.issue}</p>
+            </div>
+            )}
         </div>
       )}
     </div>
