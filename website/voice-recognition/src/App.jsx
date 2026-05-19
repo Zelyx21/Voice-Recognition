ba73853f3832aaca15d70a4b53d347c01d8611d6
Zelyx21 <beaulieu.clem.30260@gmail.com>
2026-05-19 11:33:18 +0200

    routes added on website to navigate between pages

--- website/voice-recognition/src/App.jsx
+++ website/voice-recognition/src/App.jsx
@@ -5,6 +5,7 @@ import IdentifyVoice from './IdentifyVoice.jsx'
 import Header from './header.jsx'
 import RegisterDB from './Registerdb.jsx'
 import Login from './Login.jsx'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
@@ -12,13 +13,13 @@ function App() {
 
   return (
     <>
-      <Header />
-      <IdentifyVoice />
-      <RegisterDB />
-      <Login
-        setIsAuthenticated={setIsAuthenticated}
-        setUser={setUser}
-      />
+      <Header
+        isAuthenticated={isAuthenticated} user={user} setIsAuthenticated={setIsAuthenticated} setUser={setUser} />
+      <Routes>
+        <Route path="/" element={<IdentifyVoice />} />
+        <Route path="/register" element={<RegisterDB />} />
+        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} setUser={setUser} />} />
+      </Routes>
     </>
   )
 }
