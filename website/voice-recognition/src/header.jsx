5c1961853f1c0137bf9555c149a795c444b5e8d9
Zelyx21 <beaulieu.clem.30260@gmail.com>
2026-05-19 11:33:18 +0200

    routes added on website to navigate between pages

--- website/voice-recognition/src/header.jsx
+++ website/voice-recognition/src/header.jsx
@@ -1,13 +1,38 @@
 
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './App.css'
 import dellLogo from './dell_logo.png'
 
-function Header() {
+function Header({ isAuthenticated, user, setIsAuthenticated, setUser }) {
+  const navigate = useNavigate()
+  const logout = () => {
+    setIsAuthenticated(false)
+    setUser(null)
+    navigate("/")
+  }
+
   return (
-    <main id="center">
-      <img src={dellLogo} alt="Logo DELL" className="logo" />
-      <h1>Voice Recognition</h1>
+    <main id="center" style={{ justifyContent: "space-between" }}>
+      <div style={{ display: "flex", alignItems: "center", gap: "16px" }}>
+        <img src={dellLogo} alt="Logo DELL" className="logo" onClick={() => navigate("/")} style={{ cursor: "pointer" }} />
+        <h1 onClick={() => navigate("/")} style={{ cursor: "pointer" }}>Voice Recognition</h1>
+      </div>
+
+      <div style={{ display: "flex", gap: "12px" }}>
+        {isAuthenticated ? (
+          <>
+            <p style={{ color: "var(--text-primary)" }}>Welcome, {user.name}</p>
+            <button onClick={logout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <button onClick={() => navigate("/login")}>Login</button>
+            <button onClick={() => navigate("/register")}>Register</button>
+          </>
+        )
+        }
+      </div>
     </main>
   )
 }
